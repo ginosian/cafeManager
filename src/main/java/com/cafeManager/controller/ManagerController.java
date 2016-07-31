eabd3d3bfb03e6fd5878f757faa218131ec99966
ginosian <marta.ginosian@gmail.com>
2016-07-31 16:40:40 +0400

    Illiustrates resources structure.

--- src/main/java/com/cafeManager/controller/ManagerController.java
+++ src/main/java/com/cafeManager/controller/ManagerController.java
@@ -1,7 +1,23 @@
 package com.cafeManager.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * Created by Martha on 7/29/2016.
  */
+@Controller
+@RequestMapping("/manager")
 public class ManagerController {
+    @RequestMapping(value = "home", method = RequestMethod.GET)
+    @RequestMapping(value = "/waiter", method = RequestMethod.GET)
+    @RequestMapping(value = "/waiter", method = RequestMethod.POST)
+    @RequestMapping(value = "/product", method = RequestMethod.GET)
+    @RequestMapping(value = "/product", method = RequestMethod.POST)
+    @RequestMapping(value = "/table", method = RequestMethod.GET)
+    @RequestMapping(value = "/table", method = RequestMethod.POST)
+    @RequestMapping(value = "/tables/{tableId}", method = RequestMethod.GET)
+    @RequestMapping(value = "/tables/{tableId}", method = RequestMethod.POST)
+
 }
--- src/main/java/com/cafeManager/controller/WaiterController.java
+++ src/main/java/com/cafeManager/controller/WaiterController.java
@@ -1,7 +1,17 @@
 package com.cafeManager.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * Created by Martha on 7/29/2016.
  */
+@Controller
+@RequestMapping("/waiter")
 public class WaiterController {
+
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    @RequestMapping(value = "/table/{tableId}", method = RequestMethod.GET)
+    @RequestMapping(value = "/tables/{tableId}", method = RequestMethod.POST)
 }
